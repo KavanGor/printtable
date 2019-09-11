01bbada4b50690492fb6abf68606beb26d488e3e
KavanGor <kavan@kth.se>
2019-09-11 15:00:08 +0200

    Power table editor

--- printtable.py
+++ printtable.py
@@ -3,3 +3,10 @@ def multtable(number, limit):
         print(i * number)
 
 multtable(7, 10)
+
+def powertable(power, limit):
+	for j in range(1, limit+1):
+		print(j**power)
+
+powertable(2, 4)
+powertable(3, 3)
